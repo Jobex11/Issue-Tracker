3eb8fcee2c10a15f6831913ac0d9b9a16e8122dd
jobex11 <joblawal33@gmail.com>
2023-11-11 22:01:02 +0100

    make changes

--- issue-tracker/app/Navbar.tsx
+++ issue-tracker/app/Navbar.tsx
@@ -1,23 +1,42 @@
-import React from "react";
-import Link from "next/link";
-import { AiFillBug } from "react-icons/ai";
+"use client";
 
-const Navbar = () => {
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <AiFillBug />
       </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/" className="text-zinc-500 hover:text-zinc-800">
-            Dashboard
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-800": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
           </Link>
-          <Link href="/issues">Issues</Link>
-        </li>
+        ))}
       </ul>
     </nav>
   );
 };
 
-export default Navbar;
+export default NavBar;
